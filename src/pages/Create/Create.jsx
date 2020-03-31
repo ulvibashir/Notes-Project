0f767d9c86e875b04648495a99b53d956d187a32
Ulvi Bashir <ubashirov@outlook.com>
2020-03-31 21:31:26 +0400

    basic structure

---
+++ src/pages/Create/Create.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Create() {
+    return (
+        <div>
+            Create
+        </div>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,2 @@
+export { Homepage } from './Homepage/Homepage'
+export { Create } from './Create/Create'
