b7aa2e6e3d4a1c62ea7ff981e2cd50b84f52d3f8
Ulvi Bashir <ubashirov@outlook.com>
2020-04-01 13:36:37 +0400

    GET request added, some note fixes

--- src/context/notesContext.jsx
+++ src/context/notesContext.jsx
@@ -1,66 +1,102 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import uuid from "react-uuid";  // for unique id 
+
+import { getNotes } from '../API/fetchAPI';
+
 export const NotesContext = createContext();
 
 
 // Action type
+const SET_NOTE = 'SET_NOTE';
 const ADD_NOTE = 'ADD_NOTE';
 const UPDATE_NOTE = 'UPDATE_NOTE';
 const DELETE_NOTE = 'DELETE_NOTE';
 
+// Action creators
+const setNoteA = (payload) => ({
+    type: SET_NOTE,
+    payload
+})
 
+// Reducer
 function notesReducer (state, { type, payload }){
     switch(type){
+        case SET_NOTE:
+            return {
+                ...state,
+                notes: [...state.notes, ...payload]
+            }
         default:
             return state;
     }
 }
 
 
-const initialState = {
-    notes: [
-        {
-            id: uuid(),
-            title: "Hello Mars",
-            text: "This is Sparta",
-            date: Date.now(),
-            color: "#d32727",
-            isCompleted: true
-        },
-        {
-            id: uuid(),
-            title: "Hello Jupiter",
-            text: "This is Corona",
-            date: Date.now(),
-            color: "#3a2c84",
-            isCompleted: false
-        },
-        {
-            id: uuid(),
-            title: "Hello Uganda",
-            text: "This is Uganda",
-            date: Date.now(),
-            color: "#ef8e0b",
-            isCompleted: true
-        },
-        {
-            id: uuid(),
-            title: "Hello Mozambik",
-            text: "This is Mozambik bratan",
-            date: Date.now(),
-            color: "#516f55",
-            isCompleted: false
-        }
+
+
+// const initialState = {
+//     notes: [
+//         {
+//             id: uuid(),
+//             title: "Hello Mars",
+//             text: "This is Sparta",
+//             date: Date.now(),
+//             color: "#d32727",
+//             isCompleted: true
+//         },
+//         {
+//             id: uuid(),
+//             title: "Hello Jupiter",
+//             text: "This is Corona",
+//             date: Date.now(),
+//             color: "#3a2c84",
+//             isCompleted: false
+//         },
+//         {
+//             id: uuid(),
+//             title: "Hello Uganda",
+//             text: "This is Uganda",
+//             date: Date.now(),
+//             color: "#ef8e0b",
+//             isCompleted: true
+//         },
+//         {
+//             id: uuid(),
+//             title: "Hello Mozambik",
+//             text: "This is Mozambik bratan",
+//             date: Date.now(),
+//             color: "#516f55",
+//             isCompleted: false
+//         }
         
-    ]
+//     ]
+// }
+
+
+const initialState = {
+    notes: []
 }
-
-
 
 
 export const NotesContextProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(notesReducer, initialState)
+
+
+    const setNotes = (payload) => {
+        dispatch(setNoteA(payload));
+    }
+
+
+    
+
+    useEffect(() => {
+        (async () => {
+            const response = await getNotes();
+            setNotes(response);
+        })()
+    }, [])
+    
     return(
         <NotesContext.Provider value={{...state}}>
             {children}
--- src/pages/Archive/Archive.jsx
+++ src/pages/Archive/Archive.jsx
@@ -2,25 +2,28 @@ import React, { useContext } from 'react';
 
 import { NotesContext } from '../../context/notesContext'
 import { Note } from '../../components'
-import { Row, Container } from '../../commons'
+import { Row, Container, Loader } from '../../commons'
 
 export function Archive() {
     const { notes } = useContext(NotesContext);
-    
+
     const filteredNotes = notes.filter(note => note.isCompleted !== false)
     return (
         <Container>
-            <Row>
-                {filteredNotes.map(({ id, title, text, date, color, isCompleted}) => <Note
-                    key={id}
-                    id={id}
-                    title={title}
-                    text={text}
-                    date={date}
-                    color={color}
-                    isCompleted={isCompleted}
-                />)}
-            </Row>
+            {notes.length !== 0 ?
+                <Row>
+                    {filteredNotes.map(({ id, title, text, date, color, isCompleted }) => <Note
+                        key={id}
+                        id={id}
+                        title={title}
+                        text={text}
+                        date={date}
+                        color={color}
+                        isCompleted={isCompleted}
+                    />)}
+                </Row>
+                :
+                <Loader />}
 
         </Container>
     )
